719e68b478c947cd13c21914c9654c91f48cc281
Abdulkodir Yusupov <44323682+MustanGImmortal@users.noreply.github.com>
2020-02-09 10:53:15 +0500

    First commit

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Router = express.Router();
 const todo = require('../schema/schema');
 
-Router.get('/', (req, res)=>{
+Router.get('/', (req, res) => {
     console.log(req);
     res.send("Hello World")
 });
@@ -12,14 +12,14 @@ Router.get('/todo/:chatID', function (req, res) {
     todo.find({chat_id: req.params.chatID}, function (err, users) {
         var userMap = {};
         users.forEach(function (user) {
-            userMap[user._id] = [ user.todoMessage, user.deadline] ;
+            userMap[user._id] = [user.todoMessage, user.deadline];
         });
         res.json(userMap)
     });
 });
 
 //Create Todo
-Router.post('/todo', (req, res)=>{
+Router.post('/todo', (req, res) => {
     todo.create(req.body).then(function (todo) {
         res.send(todo)
     })
